1988f1453975b04455acb9783e7303138eed0ca2
Grant <grant.macmillan94@gmail.com>
2023-09-07 11:22:47 -0400

    various changes

--- MapProject/JobMap.js
+++ MapProject/JobMap.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
+
+
 import axios from 'axios';
 
 const JobMap = ({ jobs }) => {
     const [coords, setCoords] = useState([]);
     const GEOCODING_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
-    const GOOGLE_API_KEY = 'AIzaSyDvs-pYzrss81ukHq49-um25r1ZOXK-mHo';
+    const GOOGLE_API_KEY = 'AIzaSyDvs-pYzrss81ukHq49-um25r1ZOXK-mHo'; // GRANTS API KEY - DO NOT SHARE
 
     useEffect(() => {
         async function fetchCoordsForJobs() {
-            const newCoords = [];
+            const newCoords = []; // Array of objects with id, lat, lng of each job.
 
+            // Loop through each job and fetch coordinates. store coords in newCoords array.
             for (let job of jobs) {
                 try {
+                    //Request to google API using address and API key as params.
                     const response = await axios.get(GEOCODING_API_URL, {
                         params: {
                             address: job.address,
@@ -20,14 +24,18 @@ const JobMap = ({ jobs }) => {
                         }
                     });
 
+                    // If the response is OK, store the coordinates in the newCoords array. (==='OK' means the address was found)
                     if (response.data.status === 'OK') {
                         const location = response.data.results[0].geometry.location;
+                        //add object with id, lat, lng to newCoords array.
                         newCoords.push({
                             id: job.id,
                             lat: location.lat,
                             lng: location.lng
                         });
-                    } else {
+                    }
+                    //else no address was found. Log the error. 
+                    else {
                         console.error('Error fetching coordinates:', response.data.status);
                     }
 
@@ -35,7 +43,7 @@ const JobMap = ({ jobs }) => {
                     console.error('Error:', error);
                 }
             }
-
+            // Set the coords state to the newCoords array.
             setCoords(newCoords);
         }
 

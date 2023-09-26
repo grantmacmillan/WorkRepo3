98948509b40388a5b27becf0b456029026745b45
Grant <grant.macmillan94@gmail.com>
2023-09-26 15:58:14 -0400

    getting close

--- MapProject/AddressInput.js
+++ MapProject/AddressInput.js
@@ -10,36 +10,63 @@ const AddressInput = () => {
     };
 
     return (
-        <View style={{ flex: 1, padding: 60 }}>
-            <GooglePlacesAutocomplete
-                styles={{
-                    container: {
-                        flex: 0,
-                        zIndex: 2,
-                    },
-                    listView: {
-                        position: 'absolute',
-                        top: 50,
-                    }
-                }}
-                placeholder='Enter Location'
-                onPress={(data, details = null) => {
-                    console.log(data, details);
-                    setLocation(data.description);
-                }}
-                query={{
-                    key: 'AIzaSyDvs-pYzrss81ukHq49-um25r1ZOXK-mHo', // Please replace with your actual API key
-                    language: 'en',
-                }}
-                fetchDetails={true}
-                preProcessPredictions={(predictions) => {
-                    return predictions.slice(0, 5);
-                }}
+        <View>
+            <View style={{ flex: 1, paddingVertical: 60 }}>
+                <GooglePlacesAutocomplete
+                    styles={{
+                        container: {
+                            elevation: 1,
+                            zIndex: 1000,
+                            backgroundColor: '#008000',
+                            width: '80%',
+                            alignSelf: 'center',
+                        },
+                        textInputContainer: {
+                            backgroundColor: '#008000',
+                            width: '100%',
+                        },
+                        textInput: {
+                            backgroundColor: '#008000',
+                            color: '#5d5d5d',
+                            fontSize: 16,
+                        },
+                        listView: {
+                            position: 'absolute',
+                            zIndex: 1000,
+                            top: 50,
+                            width: '80%',
+                            alignSelf: 'center',
+                            backgroundColor: '#008000', // Set background color to white or any other color
+                        },
+                        row: {
+                            zIndex: 1000,
+                            backgroundColor: '#008000', // Ensure each row is not transparent
+                        },
+                    }}
+                    placeholder='Enter Location'
+                    onPress={(data, details = null) => {
+                        console.log(data, details);
+                        setLocation(data.description);
+                    }}
+                    query={{
+                        key: 'AIzaSyDvs-pYzrss81ukHq49-um25r1ZOXK-mHo', // Please replace with your actual API key
+                        language: 'en',
+                    }}
+                    fetchDetails={true}
+                    preProcessPredictions={(predictions) => {
+                        return predictions.slice(0, 5);
+                    }}
 
-            />
-            <Button title="Submit" onPress={handlePress} />
-            {location ? <Text>Selected Location: {location}</Text> : null}
+                />
+
+            </View>
+            <View style={{ zIndex: 999 }}>
+                <Button title="Submit" onPress={handlePress} />
+                {location ? <Text>Selected Location: {location}</Text> : null}
+                <Text>Address Input</Text>
+            </View>
         </View>
+
     );
 };
 
